f2ad1e8eb58a7b1cd532a07db88db59388991299
Yaribar <yarib.soccer@gmail.com>
2021-02-17 23:19:41 -0600

    mapFloat added

--- include/Useful_Methods.h
+++ include/Useful_Methods.h
@@ -13,6 +13,7 @@ void parseString(String string_in, char* delimiter, float output_array[]);
 String buildString(float* data_to_string, uint8_t number_of_elements, char* delimiter);
 float getMean(float data[], uint8_t size);
 float getVariance(float data[], uint8_t size);
+float mapFloat(float x, float in_min, float in_max, float out_min, float out_max);
 
 #endif
 
--- src/Useful_Methods.cpp
+++ src/Useful_Methods.cpp
@@ -61,6 +61,12 @@ float getVariance(float data[], uint8_t size)
 	return variance / size;
 }
 
+float mapFloat(float x, float in_min, float in_max, float out_min, float out_max)
+{
+ return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+
 
 
 
